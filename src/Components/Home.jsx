abfcca567c4d3dd8366d0ef577d0115970e83903
Itz-Thunder <ninjalucario360@gmail.com>
2024-12-26 20:12:11 +0530

    UI Updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,27 +18,28 @@ const Home = () => {
     };
     
     return (
-        <div className='row'>
+        <div className='row justify-content-center'>
             <h1 className='text-center text-color border-bottom py-3 border-3'>Personalized QR Code Generator : </h1>
-            <div className="col-sm-1"></div>
+            
             <div className="col-sm-5 text-center">
-                <div className='main'>
-                    <input type="text" value={content} onChange={(e) => setContent(e.target.value)} className='input-field px-3' placeholder='Enter Your Text' />
-                    <button className='submit-btn' onClick={codeGenerator}>Create QR Code &nbsp;<FaArrowAltCircleRight /></button>
+                <div className='main mx-auto'>
+                    <input type="text" value={content} onChange={(e) => setContent(e.target.value)} className='input-field form-control mb-2 px-3' placeholder='Enter Your Text' />
                     {
                         error ? <p className='text-danger text-start'>Please enter some text</p> : <p></p>
                     }
+                    <button className='btn submit-btn' onClick={codeGenerator}>Create QR Code &nbsp;<FaArrowAltCircleRight /></button>
+                    
                 </div>
             </div>
             <div className="col-sm-5">
-                <div className='code-box'>
+                <div className='code-box px-4'>
                     <p className='fs-3 text-center text-color mt-4 border-bottom border-3 w-75 mx-auto pb-3'>QR Code here:</p>
                     <div className='text-center code-here'>
                         {codeVisible && <QRCodeSVG value={content} size={200}/>}
                     </div>
                 </div>
             </div>
-            <div className="col-sm-1"></div>
+            
         </div>
     );
 };
